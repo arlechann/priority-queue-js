a5d5a069adfa4a05d7f73690ee9ed51bf860ca64
arlechann <dragnov3728@gmail.com>
2021-06-21 01:34:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this
+
+Priority queue implemented with binary heap.
+
+# LICENCE
+
+CC0-1.0
+
---
+++ test/test.js
@@ -0,0 +1,6 @@
+import { assert } from 'chai';
+import PriorityQueue from '../index.js';
+
+describe('PriorityQueue', function() {
+});
+
