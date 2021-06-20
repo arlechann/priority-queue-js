f3cd79b82b36bfeefff6e39f5cbacf467a118c19
arlechann <dragnov3728@gmail.com>
2021-06-21 04:08:16 +0900

    Add push

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ export default class PriorityQueue {
 	isEmpty() {
 		return this.heap.length == 0;
 	}
+
+	_parentIndex(i) {
+		return Math.floor((i - 1) / 2);
+	}
+
+	_upHeap(i) {
+		let p = this._parentIndex(i);
+		while(i > 0 && this.compare(this.heap[p], this.heap[i])) {
+			[this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+			i = p;
+			p = this._parentIndex(i);
+		}
+	}
+
+	push(x) {
+		this._upHeap(this.heap.push(x) - 1);
+	}
 }
--- test/test.js
+++ test/test.js
@@ -32,5 +32,38 @@ describe('PriorityQueue', function() {
 			});
 		});
 	});
+
+	describe('push', function() {
+		context('when heap is empty', function() {
+			it('should be pushed', function() {
+				const pq = new PriorityQueue(comp);
+				pq.push(0);
+				assert.deepEqual(pq, { heap: [0], compare: comp });
+			});
+		});
+
+		context('when heap has elements', function() {
+			it('should be pushed and up-heaped', function() {
+				const pq = new PriorityQueue(comp);
+				pq.heap = [9, 3, 8, 1, 2, 6, 4];
+				pq.push(5);
+				assert.deepEqual(
+					pq,
+					{
+						heap: [9, 5, 8, 3, 2, 6, 4, 1],
+						compare: comp
+					}
+				);
+				pq.push(10);
+				assert.deepEqual(
+					pq,
+					{
+						heap: [10, 9, 8, 5, 2, 6, 4, 1, 3],
+						compare: comp
+					}
+				);
+			});
+		});
+	});
 });
 
