5121898d7738dbb571f1f48d860149aeac0a0079
arlechann <dragnov3728@gmail.com>
2021-06-21 04:41:24 +0900

    Add pop

--- index.js
+++ index.js
@@ -8,20 +8,47 @@ export default class PriorityQueue {
 		return this.heap.length == 0;
 	}
 
-	_parentIndex(i) {
-		return Math.floor((i - 1) / 2);
+	push(x) {
+		this._upHeap(this.heap.push(x) - 1);
+	}
+
+	pop() {
+		if(this.isEmpty()) { return undefined; }
+		this._heapSwap(0, this.heap.length - 1);
+		const ret = this.heap.pop();
+		this._downHeap(0);
+		return ret;
 	}
 
 	_upHeap(i) {
 		let p = this._parentIndex(i);
 		while(i > 0 && this.compare(this.heap[p], this.heap[i])) {
-			[this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+			this._heapSwap(p, i);
 			i = p;
 			p = this._parentIndex(i);
 		}
 	}
 
-	push(x) {
-		this._upHeap(this.heap.push(x) - 1);
+	_downHeap(i) {
+		let c = this._childrenIndexes(i);
+		while(c.length != 0) {
+			const next = c.reduce((acc, j) => this.compare(this.heap[acc], this.heap[j]) ? j : acc, i);
+			if(next == i) { break; }
+			this._heapSwap(i, next);
+			i = next;
+			c = this._childrenIndexes(next);
+		}
+	}
+
+	_parentIndex(i) {
+		return Math.floor((i - 1) / 2);
+	}
+
+	_childrenIndexes(i) {
+		return [i * 2 + 1, i * 2 + 2].filter(n => n < this.heap.length);
+	}
+
+	_heapSwap(i, j) {
+		[this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
 	}
 }
--- test/test.js
+++ test/test.js
@@ -65,5 +65,37 @@ describe('PriorityQueue', function() {
 			});
 		});
 	});
+
+	describe('pop', function() {
+		context('when heap is empty', function() {
+			it('should be undefined', function() {
+				const pq = new PriorityQueue(comp);
+				assert.isUndefined(pq.pop());
+			});
+		});
+
+		context('when heap has elements', function() {
+			it('should be popped and down-heaped', function() {
+				const pq = new PriorityQueue(comp);
+				pq.heap = [10, 9, 8, 5, 2, 6, 4, 1, 3];
+				assert.equal(pq.pop(), 10);
+				assert.deepEqual(
+					pq,
+					{
+						heap: [9, 5, 8, 3, 2, 6, 4, 1],
+						compare: comp
+					}
+				);
+				assert.equal(pq.pop(), 9);
+				assert.deepEqual(
+					pq,
+					{
+						heap: [8, 5, 6, 3, 2, 1, 4],
+						compare: comp
+					}
+				);
+			});
+		});
+	});
 });
 
