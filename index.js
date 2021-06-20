6c29c32c8914ccb9d04b7db4aed81b073f83fb0d
arlechann <dragnov3728@gmail.com>
2021-06-21 02:00:50 +0900

    Add constructor

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 export default class PriorityQueue {
+	constructor(comp) {
+		this.heap = [];
+		this.compare = comp;
+	}
 }
--- test/test.js
+++ test/test.js
@@ -2,5 +2,18 @@ import { assert } from 'chai';
 import PriorityQueue from '../index.js';
 
 describe('PriorityQueue', function() {
+	const comp = (a, b) => a < b;
+
+	describe('constructor', function() {
+		it('should be empty', function() {
+			const pq = new PriorityQueue(comp);
+			assert.deepEqual(pq.heap, []);
+		});
+
+		it('should be set given function', function() {
+			const pq = new PriorityQueue(comp);
+			assert.equal(pq.compare, comp);
+		});
+	});
 });
 
